cd9fe8b1d319fda08c4ace9b3e0b6bc08b1f904d
Taiyo FUJII <taiyoster@gmail.com>
2021-04-21 10:26:14 +0900

    Textlint prh added.

--- dict/dict.js
+++ dict/dict.js
@@ -1,4 +1,5 @@
 module.exports = [
+    // https://azu.github.io/morpheme-match/
     // ルール　助詞の連続（タイプミス）
     {
         message: `「$1$2$3」には助詞の連続があります（単純な助詞の連続）`,
@@ -72,6 +73,171 @@ module.exports = [
                 "pos_detail_1": "係助詞",
                 "_capture": "$3"
             }
+        ]
+    },
+    {
+        message: '「~た~た」と続いています',
+        tokens: [
+            {
+                "pos": "動詞",
+                "conjugated_form": "連用形",
+            },{
+                "surface_form": "た",
+                "pos": "助動詞",
+                "conjugated_form": "基本形",
+        
+            },{
+                "pos": "動詞",
+                "conjugated_form": "連用形",
+            },{
+                "surface_form": "た",
+                "pos": "助動詞",
+            }
+        ]
+    },
+    {
+        message: '「~れ。」で終わっています。~れる。~れた。などと続く可能性があります',
+        tokens: [
+            {
+                "pos": "動詞",
+                "conjugated_form": "未然形",
+            },
+            {
+                "surface_form": "れ",
+                "pos": "動詞",
+                "pos_detail_1": "接尾",
+                "conjugated_form": "連用形",
+                "basic_form": "れる",
+            },
+            {
+                "surface_form": "。",
+                "pos_detail_1": "句点",
+            }
+        ]
+    },
+    {
+        message: '連用形の動詞で文が終わっています。~た。~ていた。などと続く可能性があります',
+        tokens: [
+            {
+                "pos": "動詞",
+                "conjugated_form": "連用形",
+            },
+            {
+                "surface_form": "。",
+            }
+        ]
+    },
+    // 超えると越える
+    {
+        message: '距離・時間・点は「越える」を用います',
+        tokens: [
+            {
+                "surface_form": "日",
+                "pos_detail_1": "接尾",
+                "pos_detail_2": "助数詞",
+            },
+            {
+                "surface_form": "を",
+                "pos": "助詞",
+                "pos_detail_1": "格助詞",
+            },
+            {
+                "surface_form": "超え",
+                "pos": "動詞",
+            },
+        ]
+    },
+    {
+        message: '距離・時間・点は「越える」を用います',
+        tokens: [
+            {
+                "surface_form": "時間",
+                "pos_detail_1": "接尾",
+                "pos_detail_2": "助数詞",
+            },
+            {
+                "surface_form": "を",
+                "pos": "助詞",
+                "pos_detail_1": "格助詞",
+            },
+            {
+                "surface_form": "超え",
+                "pos": "動詞",
+            },
+        ]
+    },
+    {
+        message: '距離・時間・点は「越える」を用います',
+        tokens: [
+            {
+                "surface_form": "年",
+                "pos_detail_1": "接尾",
+                "pos_detail_2": "助数詞",
+            },
+            {
+                "surface_form": "を",
+                "pos": "助詞",
+                "pos_detail_1": "格助詞",
+            },
+            {
+                "surface_form": "超え",
+                "pos": "動詞",
+            },
+        ]
+    },
+    {
+        message: '距離・時間・点は「越える」を用います',
+        tokens: [
+            {
+                "surface_form": "半年",
+            },
+            {
+                "surface_form": "を",
+                "pos": "助詞",
+                "pos_detail_1": "格助詞",
+            },
+            {
+                "surface_form": "超え",
+                "pos": "動詞",
+            },
+        ]
+    },
+    {
+        message: '距離・時間・点は「越える」を用います',
+        tokens: [
+            {
+                "surface_form": "分",
+                "pos_detail_1": "接尾",
+                "pos_detail_2": "助数詞",
+            },
+            {
+                "surface_form": "を",
+                "pos": "助詞",
+                "pos_detail_1": "格助詞",
+            },
+            {
+                "surface_form": "超え",
+                "pos": "動詞",
+            },
+        ]
+    },
+    {
+        message: '距離・時間・点は「越える」を用います',
+        tokens: [
+            {
+                "surface_form": "秒",
+                "pos_detail_1": "接尾",
+                "pos_detail_2": "助数詞",
+            },
+            {
+                "surface_form": "を",
+                "pos": "助詞",
+                "pos_detail_1": "格助詞",
+            },
+            {
+                "surface_form": "超え",
+                "pos": "動詞",
+            },
         ]
     },
 ];
